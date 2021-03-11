2b2b96987a9df38e2c78b60ab5e6661f30441fd0
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2021-03-11 15:07:09 +0500

    App component rendered using TDD

--- src/App/tests/App.test.js
+++ src/App/tests/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from './../App'
+import App from '../App'
 describe("<App />", () => {
   it("Renders <App /> component correctly", () => {
     render(<App />);
