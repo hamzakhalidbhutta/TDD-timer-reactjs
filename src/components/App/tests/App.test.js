38abda932caa14b8d53fa21cd96b86d53c41c304
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2021-03-11 14:33:08 +0500

    Refactored and Successfully rendered the app component using TDD

---
+++ src/components/App/tests/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from './../App'
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    render(<App />);
+    expect(
+      screen.getByText(/App/i)
+    ).toBeInTheDocument;
+  });
+});
