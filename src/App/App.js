4169390f1785fb237108a039fb05e457390685a7
Hamza Khalid Bhutta <hamzakhalidbhutta430@gmail.com>
2021-03-11 17:35:33 +0500

    Enzyme added and test Successfully added

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
     return (
         <div>
-          <h1>App</h1>  
+          <h1>Timer</h1>  
         </div>
     )
 }
--- src/App/tests/App.test.js
+++ src/App/tests/App.test.js
@@ -1,10 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import App from '../App'
+import React from 'react'
+import { shallow } from "enzyme";
+import App from './../App'
+
+
 describe("<App />", () => {
   it("Renders <App /> component correctly", () => {
-    render(<App />);
-    expect(
-      screen.getByText(/App/i)
-    ).toBeInTheDocument;
+    const conatiner = shallow(<App />);
+    expect(conatiner.find('h1').length).toEqual(1);
   });
+
+  // it("Should contain the <Timer /> Component"),()=>{
+
+  // }
 });
